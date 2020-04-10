8f67280c352b50bdc1a0c524c95835a1a25aee6f
Jan Holan <jan.holan@h2net.cz>
2020-04-10 09:11:38 +0200

    Edit ApplicationContentRootResolver

--- WindowsServiceCoreSample/Internal/ApplicationContentRootResolver.cs
+++ WindowsServiceCoreSample/Internal/ApplicationContentRootResolver.cs
@@ -8,43 +8,15 @@ namespace WindowsServiceCoreSample.Internal
         public static string GetApplicationContentRoot()
         {
             string currentDirectory = Directory.GetCurrentDirectory();
+            string applicationBasePath = AppContext.BaseDirectory;
 
-            string rootDirectory = ResolveApplicationRootDirectory(currentDirectory, nameof(WindowsServiceCoreSample));
-            if (rootDirectory == null)
+            //Try find appsettings.json file in applicationBasePath, to take applicationBasePath instead of currentDirectory (eg.: for Windows Services where currentDirectory is in 'C:\Windows\system32\')
+            if (File.Exists(Path.Combine(applicationBasePath, "appsettings.json")))
             {
-                string applicationBasePath = AppContext.BaseDirectory;
-                rootDirectory = ResolveApplicationRootDirectory(applicationBasePath, nameof(WindowsServiceCoreSample));
-            }
-
-            if (rootDirectory != null)
-            {
-                return rootDirectory;
+                return applicationBasePath;
             }
 
             return currentDirectory;
-        }
-
-        private static string ResolveApplicationRootDirectory(string path, string applicationProjectName)
-        {
-            //Try find appsettings.json file or <applicationProjectName>\appsettings.json from
-            var di = new DirectoryInfo(path);
-
-            while (di.Parent != null)
-            {
-                if (File.Exists(Path.Combine(di.FullName, "appsettings.json")))
-                {
-                    return di.FullName;
-                }
-
-                if (File.Exists(Path.Combine(di.FullName, applicationProjectName, "appsettings.json")))
-                {
-                    return Path.Combine(di.FullName, applicationProjectName);
-                }
-
-                di = di.Parent;
-            }
-
-            return null;
         }
     }
 }
